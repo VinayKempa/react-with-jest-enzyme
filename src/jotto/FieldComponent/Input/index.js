d63dc990a329830426ea2e9d919ea4b68736a17d
Vinay Madenahally Prasad <vinaymp25@gmail.com>
2021-10-22 12:27:28 +0530

    1. Simulating OnChange 2. Mocking useState 3. Mocking setState function

--- src/jotto/FieldComponent/Input/index.js
+++ src/jotto/FieldComponent/Input/index.js
@@ -1,7 +1,31 @@
 import React from "react";
 
-const Input = (props) => {
-  return <div data-test="component-input"></div>;
+const Input = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState("");
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="Enter Guess"
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          type="submit"
+          className="btn btnprimary mb-2"
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Input;
--- src/jotto/FieldComponent/Input/index.test.js
+++ src/jotto/FieldComponent/Input/index.test.js
@@ -16,3 +16,17 @@ test("Input renders without errors", () => {
   const inputComponent = findByTestAttr(wrapper, "component-input");
   expect(inputComponent.length).toBe(1);
 });
+
+describe("State controlled input field", () => {
+  test("state updates with value of input box upon change", () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+});
